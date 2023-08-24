857c4969581aa2b8a5c00f960366cb1543546b4b
Yashar Honarmandi <yashar@entecon.se>
2023-08-24 14:58:50 +0200

    Add various corrections and cleanup

--- init.el
+++ init.el
@@ -1,34 +1,38 @@
 ;; .emacs.d/init.el
 
 ;; ===================================
-;; MELPA Package Support
+;; Custom variables
 ;; ===================================
-;; Enables basic packaging support
-(require 'package)
 
-;; Adds the Melpa archive to the list of available repositories
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
+(defvar myBackupDirectory "/my/backup/path")
 
-;; Initializes the package infrastructure
-(package-initialize)
-
-;; If there are no archived package contents, refresh them
-(when (not package-archive-contents)
-  (package-refresh-contents))
-
-;; Installs packages
-;;
-;; myPackages contains a list of package names
+; myPackages contains a list of package names
 (defvar myPackages
-  '(;;better-defaults                 ;; Set up some better Emacs defaults
-    ;;material-theme                  ;; Theme
-    elpy                              ;; Emacs Lisp Python Environment
+  '(material-theme                  ;; Theme
+    ;better-defaults                ;; Changed defaults for Emacs. Should be added to own file instead
+    elpy                            ;; Emacs Lisp Python Environment
     )
   )
 
-;; Scans the list in myPackages
-;; If the package listed is not already installed, install it
+;; ===================================
+;; MELPA Package Support
+;; ===================================
+; Enables basic packaging support
+(require 'package)
+
+; Adds the Melpa archive to the list of available repositories
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+
+; Initializes the package infrastructure
+(package-initialize)
+
+; If there are no archived package contents, refresh them
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+; Installs packages by scanning the list in myPackages
+; If the package listed is not already installed, install it
 (mapc #'(lambda (package)
           (unless (package-installed-p package)
             (package-install package)))
@@ -38,8 +42,8 @@
 ;; Basic Customization
 ;; ===================================
 
-;(setq inhibit-startup-message t)    ;; Hide the startup message
-;(load-theme 'material t)            ;; Load material theme
+(setq inhibit-startup-message t)     ;; Hide the startup message
+(load-theme 'material t)             ;; Load material theme
 (global-linum-mode t)                ;; Enable line numbers globally
 (desktop-save-mode 1)                ;; Save session
 (setq visible-bell 1)                ;; Mute bell
@@ -49,36 +53,52 @@
 ;; Advanced Customization
 ;; ===================================
 
-;; (backup-directory-alist (quote (("." . "/mnt/54A09841A0982B8C/Documents/Backup")))) ;; Store backupfiles in specific directory
-
-;; Set backup location
+; Set backup location
 (setq
    backup-by-copying t      ; don't clobber symlinks
    backup-directory-alist
-    '(("." . "/mnt/54A09841A0982B8C/Emacs/Backup"))    ; don't litter my fs tree
+    `(("." . ,myBackupDirectory))    ; don't litter my fs tree
+   auto-save-file-name-transforms
+    `((".*" ,myBackupDirectory t))
    delete-old-versions t
    kept-new-versions 1
    kept-old-versions 3
    version-control t)       ; use versioned backups
 
+; Delete old backup files
+(message "Deleting old backup files...")
+(let ((week (* 60 60 24 7))
+      (current (float-time (current-time))))
+  (dolist (file (directory-files temporary-file-directory t))
+    (when (and (backup-file-name-p file)
+               (> (- current (float-time (fifth (file-attributes file))))
+                  week))
+      (message "%s" file)
+      (delete-file file))))
+
 ;; ====================================
 ;; Development Setup
 ;; ====================================
-;; Enable elpy
+; Enable elpy
 (elpy-enable)
 
-(setq python-shell-interpreter "/usr/bin/python3"
-      python-shell-interpreter-args "-i")
+; Might need to run elpy-rpc-reinstall-virtualenv
+; Make sure to install virtualenv!
+(setq
+      python-shell-interpreter "python3"
+      python-shell-interpreter-args "-i"
+      elpy-rpc-python-command "python3"
+       )
 
-(setq elpy-rpc-python-command "python3")
-
+;; ====================================
 ;; User-Defined init.el ends here
+;; ====================================
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(elpy)))
+ '(package-selected-packages '(elpy material-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
