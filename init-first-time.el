6487ba5fc037c98d4841b99344f5d07a2d52db9f
Yashar Honarmandi <yashar@entecon.se>
2024-08-16 15:10:09 +0200

    Update first-time init script

--- init-first-time.el
+++ init-first-time.el
@@ -2,4 +2,8 @@
 ;; Necessary one-time function calls
 ;; ===================================
 
+; Ensure virtualenv is in place
 (elpy-rpc-reinstall-virtualenv)
+
+; Install language server for omnisharp
+(mnisharp-install-server)
