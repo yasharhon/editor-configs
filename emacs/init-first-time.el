5450d4973649f085dd2818c304ce62a33394798a
Yashar Honarmandi <yashar@honarmandi.se>
2026-01-25 01:20:17 +0100

    Update first-time init

--- emacs/init-first-time.el
+++ emacs/init-first-time.el
@@ -1,6 +1,3 @@
 ;; ===================================
 ;; Necessary one-time function calls
 ;; ===================================
-
-; Ensure virtualenv is in place
-(elpy-rpc-reinstall-virtualenv)
