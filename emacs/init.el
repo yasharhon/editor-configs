de20298e7412610addf7b94fcce535cf8c4690f8
Yashar Honarmandi <yashar@honarmandi.se>
2026-08-14 16:48:24 +0200

    Move autogenerated things to custom init file

--- emacs/init.el
+++ emacs/init.el
@@ -109,7 +109,6 @@
     )
    )
   )
-)
 
 ; LSP Latex - LSP support for TeX documents
 (use-package lsp-latex
@@ -174,17 +173,10 @@
 ;; ====================================
 
 ;; ====================================
-;; User-Defined init.el ends here
+;; Delegation of auto-generated statements
 ;; ====================================
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(company material-theme)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file)
+  )
